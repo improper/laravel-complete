547b7fae2d2568acf6437edf37227dab273e31c1
Tyler Mills <tylerssn@gmail.com>
2023-01-07 01:45:44 -0500

    Add Lando configuration with Vite compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        basicSsl(), // Ensure secure lando request can connect to vite
     ],
+    server: {
+        https: true,
+        host: true,
+        port: 3009,
+        hmr: {host: 'localhost', protocol: 'wss'},
+    },
 });
